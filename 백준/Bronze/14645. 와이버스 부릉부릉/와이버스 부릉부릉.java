78ff8424a2e45882693ea78ac025e97e13e93c92
Eun Seok Lee <107795925+silverstone2@users.noreply.github.com>
2022-10-22 23:08:04 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 204 ms, Memory: 17268 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 출발역과 종착역을 제외한 정거장의 수 n 입력
+		int n = sc.nextInt();
+		// 출발역에서 탑승하는 사람의 수 k 입력
+		int k = sc.nextInt();
+		// 버스 운전수 이름을 driver 변수에 저장
+		String driver = "비와이";
+		// 버스운전수 이름 출력
+		System.out.println(driver);
+	}
+}
