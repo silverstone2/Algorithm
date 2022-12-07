9aee9bfcac80d7824df6d835f1369f0c85a1f96d
Eun Seok Lee <107795925+silverstone2@users.noreply.github.com>
2022-12-07 11:27:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 732 ms, Memory: 66112 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr=new int[n][2];
+        for(int i = 0; i<n;i++){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            arr[i][0]=Integer.parseInt(st.nextToken());
+            arr[i][1]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, (a, b) -> {
+            if(a[0] == b[0]){
+                return a[1] - b[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+
+        for(int i = 0; i<n;i++){
+            sb.append(arr[i][0]+" "+arr[i][1]+"\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
