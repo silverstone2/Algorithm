af09ba00706613a8053f51cadd0f1d55d28da551
Eun Seok Lee <107795925+silverstone2@users.noreply.github.com>
2023-01-01 23:36:02 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 256 ms, Memory: 18128 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,10 @@
+import java.util.Calendar;
+
+public class Main {
+	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance();
+		System.out.println(cal.get(cal.YEAR));
+		System.out.println(cal.get(cal.MONTH)+1);
+		System.out.println(cal.get(cal.DAY_OF_MONTH));
+	}
+}
