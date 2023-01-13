967152cace20a4b45ab192185e0ad886dae8e963
Eun Seok Lee <107795925+silverstone2@users.noreply.github.com>
2023-01-13 23:48:05 +0900

    [Bronze IV] Title: Which Alien?, Time: 204 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int antenna = sc.nextInt();
+		int eyes = sc.nextInt();
+		if(antenna >= 3 && eyes <=4)
+			System.out.println("TroyMartian");
+		if(antenna <= 6 && eyes >= 2)
+			System.out.println("VladSaturnian");
+		if(antenna <= 2 && eyes <= 3)
+			System.out.println("GraemeMercurian");
+	}
+}
