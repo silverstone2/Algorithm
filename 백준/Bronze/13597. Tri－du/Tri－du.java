969e472e02aedd60b87ba4e7db784af2e68d9060
Eun Seok Lee <107795925+silverstone2@users.noreply.github.com>
2023-01-20 23:50:37 +0900

    [Bronze IV] Title: Tri-du, Time: 204 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/13597. Tri－du/Tri－du.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	 public static void main(String[] args) {
+	       Scanner sc = new Scanner(System.in); 
+	       int a=sc.nextInt();
+	       int b=sc.nextInt();
+	       
+	       if(a==b) {System.out.println(a);}
+	       else {
+	           if(a>b) {
+	               System.out.println(a);
+	           }
+	           else {
+	               System.out.println(b);
+	           }
+	               
+	       }
+	 }
+}
