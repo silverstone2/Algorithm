16d765249248935c9e6fe009c80396e16d93df8a
Eun Seok Lee <107795925+silverstone2@users.noreply.github.com>
2023-01-26 23:49:53 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 212 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){                        
+        Scanner sc = new Scanner(System.in);        
+        long x = sc.nextLong(), y = sc.nextLong();
+        System.out.println(x * y / 2);
+    }
+}
