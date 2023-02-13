4fe31f58a0fa5e9328e0c836ec261bf3b99940f5
Eun Seok Lee <107795925+silverstone2@users.noreply.github.com>
2023-02-13 18:38:24 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 65.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int size = message.length();
+        answer = size * 2;
+        return answer;
+    }
+}
