eb2017d473b4f3e8d3b5c74657f77b37c2f4cab8
Eun Seok Lee <107795925+silverstone2@users.noreply.github.com>
2023-02-14 23:44:27 +0900

    [level 0] Title: 배열의 평균값, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        double sum = 0;
+        for(int i=0; i<numbers.length; i++) {
+            sum += numbers[i];
+        }
+        answer = sum / (double)numbers.length;
+        return answer;
+    }
+}
