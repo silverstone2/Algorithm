92ccf77afa1cea8adde0c286af9243bafa04c96a
Eun Seok Lee <107795925+silverstone2@users.noreply.github.com>
2023-03-04 15:14:10 +0900

    [Silver III] Title: 1로 만들기, Time: 220 ms, Memory: 21720 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] dp = new int[n+1];
+		
+		
+		/*
+		 * 점화식
+		 *  - 1를 뺄 때는 dp[i] = dp[i-1] + 1
+			- 2로 나눌 수 있을 때는 dp[i] = dp[i/2] + 1
+			- 3으로 나눌 수 있을 때는 dp[i] = dp[i/3] + 1
+		 
+		 */
+		dp[1] = 0;
+		for(int i=2; i<n+1; i++) {
+			dp[i] = dp[i-1] + 1;
+			
+			if(i%2 == 0 && dp[i] > dp[i/2]+1) {
+				dp[i] = dp[i/2]+1;
+			}
+			if(i%3 == 0 && dp[i] > dp[i/3]+1) {
+				dp[i] = dp[i/3]+1;
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}
